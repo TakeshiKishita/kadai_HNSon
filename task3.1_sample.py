603e90f1debdc1c7714f41172f89fe7b4740b350
Takeshi Kishita <xxtkcxx@gmail.com>
2017-10-02 18:49:50 +0900

    add kadai file

---
+++ task3.1_sample.py
@@ -0,0 +1,14 @@
+def make_divisor_list(num):
+    if num < 1:
+        return []
+    elif num == 1:
+        return [1]
+    else:
+        divisor_list = []
+        divisor_list.append(1)
+        for i in range(2, num // 2 + 1):
+            if num % i == 0:
+                divisor_list.append(i)
+        divisor_list.append(num)
+
+        return divisor_list
