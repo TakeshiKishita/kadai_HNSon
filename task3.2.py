cfdfb8aa78867662d032060fca23203cd9f16c0a
Takeshi Kishita <xxtkcxx@gmail.com>
2017-10-04 18:29:31 +0900

    update

--- task3.2.py
+++ task3.2.py
@@ -13,12 +13,53 @@ def Factoring (num):
 
 	count = 2
 
-	if num <= 1:
-		#1以外の数字の場合を
-		return print('素数ではありません')
+	if num.isdigit() or num < 2:
+		#自然数かつ1以上の数値を判定
+		return print('１より大きい自然数を入力してください')
+
+	sequence_list = [i for i in range (2, num+1)]
+	#2から入力値までの数列のリストを作成
+	prime_list = []
+	#素数のリスト
+
+	while True:
+		prime = min(sequence_list)
+
+		if prime > math.sqrt(num):
+			#入力値の平方根以上を全て代入する
+			prime_list.extend(sequence_list)
+			break
+
+		while i < len(sequence_list):
+			if num % i == 0:
+				prime_list.append(i)
 
 
 
 
 
 
+def eratosthenes(limit):
+	A = [i for i in range(2, limit+1)]
+	P = []
+	time = 0
+
+	while True:
+		prime = min(A)
+
+		if prime > math.sqrt(limit):
+			break
+
+		P.append(prime)
+
+		i = 0
+		while i < len(A):
+			if A[i] % prime == 0:
+				A.pop(i)
+				continue
+			i += 1
+
+	for a in A:
+		P.append(a)
+
+	return len(P)
