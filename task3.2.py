674f0cdbf8a0c0a51ca33a226c4c66070312a4af
Takeshi Kishita <xxtkcxx@gmail.com>
2017-10-05 16:02:07 +0900

    add get_prime_number

--- task3.2.py
+++ task3.2.py
@@ -2,7 +2,25 @@
 # ②-1素因数分解を再帰的に行う関数を作成せよ。
 
 #coding:utf-8
-def Factoring (num):
+
+file_name = "file.txt"
+
+f = open(file)
+reader = f.readlines() # 1行毎にファイル終端まで全て読む(改行文字も含まれる)
+f.close()
+# lines2: リスト。要素は1行の文字列データ
+
+for line in lines2:
+	print line,
+print
+
+
+
+
+
+
+
+def get_prime_number (num):
 	"""Summary line.
 	エラトステネスの篩アルゴリズムを使い、素因数分解を行う
 	Args:
@@ -11,14 +29,12 @@ def Factoring (num):
 		list: 素因数分解された素数
 	"""
 
-	count = 2
-
-	if num.isdigit() or num < 2:
+	if not str(num).isdigit() or num < 2:
 		#自然数かつ1以上の数値を判定
 		return print('１より大きい自然数を入力してください')
 
 	sequence_list = [i for i in range (2, num+1)]
-	#2から入力値までの数列のリストを作成
+	#2から入力値までの数列のリスト
 	prime_list = []
 	#素数のリスト
 
@@ -26,40 +42,22 @@ def Factoring (num):
 		prime = min(sequence_list)
 
 		if prime > math.sqrt(num):
-			#入力値の平方根以上を全て代入する
+			#入力値の平方根以上は全てリストに加えて終了
 			prime_list.extend(sequence_list)
 			break
-
-		while i < len(sequence_list):
-			if num % i == 0:
-				prime_list.append(i)
-
-
-
-
-
-
-def eratosthenes(limit):
-	A = [i for i in range(2, limit+1)]
-	P = []
-	time = 0
-
-	while True:
-		prime = min(A)
-
-		if prime > math.sqrt(limit):
-			break
-
-		P.append(prime)
+		else:
+			prime_list.append(prime)
 
 		i = 0
-		while i < len(A):
-			if A[i] % prime == 0:
-				A.pop(i)
+		while i < len(sequence_list):
+			if sequence_list[i] % prime == 0:
+				#素数の倍数をリストから削除
+				sequence_list.pop(i)
 				continue
 			i += 1
 
-	for a in A:
-		P.append(a)
+	return prime_list
 
-	return len(P)
+num = 12
+ans = get_prime_number(num)
+print(ans)
