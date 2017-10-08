dbacaeca86b99958f785ceb6d96512871540e6c0
Takeshi Kishita <tkc@MacBookPro.local>
2017-10-08 15:17:45 +0900

    update task3.2 3.2

--- task3.1.py
+++ task3.1.py
@@ -1,2 +1,100 @@
-def sum_divisor(x):
+# 課題3.【プログラミング】
+# ①	変数int xを引数として渡されると、xの約数を全て足し算した結果を返すプログラムを作成せよ。
 
+# coding:utf-8
+import math
+
+def get_prime_number(num):
+	"""Summary line.
+	エラトステネスの篩を使い、素数を列挙する
+	Args:
+		num (int): 素因数分解を行う数値
+	Returns:
+		list: numまでの素数リスト
+	"""
+
+	sequence_list = [i for i in range(2, num + 1)]
+	#2から入力値までの数列のリスト
+
+	prime_list = []
+	# 素数のリスト
+
+	while True:
+		prime = min(sequence_list)
+
+		if prime > math.sqrt(num):
+			# 入力値の平方根以上は全てリストに加えて終了
+			prime_list.extend(sequence_list)
+			break
+		else:
+			prime_list.append(prime)
+
+		i = 0
+		while i < len(sequence_list):
+			if sequence_list[i] % prime == 0:
+				# 素数の倍数をリストから削除
+				sequence_list.pop(i)
+				continue
+			i += 1
+	return prime_list
+
+def prime_factorization(num):
+	"""Summary line.
+	素数を使い、素因数分解を行う
+	Args:
+		num (int): 素因数分解を行う数値
+	Returns:
+		list: 素因数分解の解
+	"""
+
+	prime_list = get_prime_number(num)
+	# ２からnumまでの素数リストを作成
+
+	temp_num = num
+	ans_list = []
+	i = 0
+	while num >= prime_list[i]**2:
+		# 対象数値の平方根の数まで処理を行う
+		if temp_num % prime_list[i] == 0:
+			ans_list.append(str(prime_list[i]))
+			temp_num /= prime_list[i]
+		else:
+			i += 1
+
+	if temp_num != 1:
+		ans_list.append(str(int(temp_num)))
+
+	return ans_list
+
+
+try:
+	while True:
+		num = int(input())
+		prime_list = prime_factorization(num)
+		#素因数分解のリストを作成
+
+		temp_num = 0
+		power_num = 0
+		sum_divisor = 0
+		for i, val in enumerate(prime_list):
+			#素因数分解を用いて約数の総和を求める公式
+			if i == 0:
+				temp_num = 1 + int(val)
+				power_num = 2
+			else:
+				if val == prime_list[i-1]:
+					#素数が直前と同じだった場合
+					temp_num += int(val)**power_num
+					power_num += 1
+				else:
+					if sum_divisor == 0:
+						sum_divisor = temp_num
+					else:
+						sum_divisor *= temp_num
+					temp_num = 1 + int(val)
+					power_num = 2
+
+		sum_divisor *= temp_num
+		print(sum_divisor)
+except EOFError:
+	pass
--- task3.2.py
+++ task3.2.py
@@ -1,9 +1,10 @@
 # 課題3.【プログラミング】
 # ②-1素因数分解を再帰的に行う関数を作成せよ。
 
-#coding:utf-8
+# coding:utf-8
+import math
 
-def get_prime_number (num):
+def get_prime_number(num):
 	"""Summary line.
 	エラトステネスの篩を使い、素数を列挙する
 	Args:
@@ -12,16 +13,17 @@ def get_prime_number (num):
 		list: numまでの素数リスト
 	"""
 
-	sequence_list = [i for i in range (2, num+1)]
+	sequence_list = [i for i in range(2, num + 1)]
 	#2から入力値までの数列のリスト
+
 	prime_list = []
-	#素数のリスト
+	# 素数のリスト
 
 	while True:
 		prime = min(sequence_list)
 
 		if prime > math.sqrt(num):
-			#入力値の平方根以上は全てリストに加えて終了
+			# 入力値の平方根以上は全てリストに加えて終了
 			prime_list.extend(sequence_list)
 			break
 		else:
@@ -30,30 +32,31 @@ def get_prime_number (num):
 		i = 0
 		while i < len(sequence_list):
 			if sequence_list[i] % prime == 0:
-				#素数の倍数をリストから削除
+				# 素数の倍数をリストから削除
 				sequence_list.pop(i)
 				continue
 			i += 1
 	return prime_list
 
+
 def check_num_error(num):
 	"""Summary line.
 	文字列の自然数判定
 	Args:
-		num (str): 判定するう数値（文字列）
+		num (str): 判定する数値（文字列）
 	Returns:
 		bool: True/2以上の自然数 False/その他
 	"""
-	ans = False
 	if not num.isdigit():
-		#文字列が数値かつ自然数ではない
+		# 文字列が数値かつ自然数ではない
 		ans = False
 	elif int(num) < 2:
-		#2より小さい自然数
+		# 2より小さい自然数
 		ans = False
 	else:
 		ans = True
 
+	return ans
 
 def prime_factorization(num):
 	"""Summary line.
@@ -61,49 +64,48 @@ def prime_factorization(num):
 	Args:
 		num (int): 素因数分解を行う数値
 	Returns:
-		str: 素因数分解の解
+		list: 素因数分解の解
 	"""
 
-	ans = ""
 	prime_list = get_prime_number(num)
-	#２からnumまでの素数リストを作成
+	# ２からnumまでの素数リストを作成
 
-	square_root_number = num**0.5
-	#引数の平方根
-
+	temp_num = num
+	ans_list = []
 	i = 0
-	while i > (square_root_number)+1:
-		#対象数値の平方根の数まで処理を行う
-		if num % prime_list[i] == 0:
-			if ans == "":
-				ans = str(i)
-			else:
-				ans += ","+str(i)
-			num /= prime_list[i]
+	while num >= prime_list[i]**2:
+		# 対象数値の平方根の数まで処理を行う
+		if temp_num % prime_list[i] == 0:
+			ans_list.append(str(prime_list[i]))
+			temp_num /= prime_list[i]
 		else:
 			i += 1
-	return ans
+
+	if temp_num != 1:
+		ans_list.append(str(int(temp_num)))
+
+	return ans_list
 
 
-file_name = "file.txt"
+file_name = "test.txt"
 with open(file_name, "r") as f:
-	#ファイルの内容を読み込む
+	# ファイルの内容を読み込む
 	reader = f.readlines()
 
 for i, val in enumerate(reader):
-	#ファイルの内容1行ごとに素因数分解を行う
-	ans = ''
+	# ファイルの内容1行ごとに素因数分解を行う
+	ans_list = []
 	num = val.replace('\n', '')
 
 	if check_num_error(num):
-		ans = prime_factorization(int(num))
-		#素因数分解
+		# 正しい引数だった場合、素因数分解
+		ans_list = prime_factorization(int(num))
 	else:
-		ans = "ERROR"
+		ans_list = ["ERROR"]
 
-	reader[i] = num+","+str(ans)+"\n"
-	#行の成形
+	reader[i] = num + "," + ",".join(ans_list) + "\n"
+# 行の成形
 
 with open(file_name, "w") as f:
-	#ファイルへ書き出す
+	# ファイルへ書き出す
 	f.writelines(reader)
